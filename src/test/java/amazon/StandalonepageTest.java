9b64e6dcc7945f02cc709c2255cc260bd62a1d07
Nagarajupalli Jagadeesh <njagadeesh@BLR.ALLEGISINDIA.COM>
2026-03-22 01:24:53 +0530

    added code pom

--- src/test/java/amazon/StandalonepageTest.java
+++ src/test/java/amazon/StandalonepageTest.java
@@ -7,15 +7,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class StandalonepageTest {
     
-	@Test
     public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless=new");
+	    options.addArguments("--no-sandbox");
+	    options.addArguments("--disable-dev-shm-usage");
+
+	    WebDriver driver = new ChromeDriver(options);
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in/");
@@ -25,5 +35,6 @@ public class StandalonepageTest {
 		String title  =  driver.getTitle();
 		System.out.println("We are on page " + title);
 		driver.quit();
+		
 	}
 }
